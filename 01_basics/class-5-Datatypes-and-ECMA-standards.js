259c20248232d6efe8677ef1c580ea64189f29a3
muhammad umair <muhammadumairabbasi637@gmail.com>
2024-10-05 11:23:11 +0500

    data types class completed

---
+++ 01_basics/class-5-Datatypes-and-ECMA-standards.js
@@ -0,0 +1,26 @@
+// Topic Datatypes in js
+
+"use stricht"; // treat all js code as newer version
+
+//  Note: In programming code readability should be high
+
+// Data types
+
+// 1 There are seven primitive data types in js
+// NN SS BB U
+
+// examples
+
+/*
+let number = 10; // 2 to power 53
+let Null = null; // empty stand alone value
+let symb = Symbol(123); // for uniqueness
+let str = "Umair";
+let bool = true;
+let bigInt = 87687346865894483n; // when num is to large
+let Undefined = undefined; // not defined value yet
+*/
+
+// Non primitive data types 
+
+// 1 Object
